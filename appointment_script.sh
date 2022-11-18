3f53f35386b091fc5e54d4422dfb0e92ef79b71b
Manuel Pedraza <111818883+MPedrazam@users.noreply.github.com>
2022-11-17 21:24:48 -0500

    Update appointment_script.sh

--- appointment_script.sh
+++ appointment_script.sh
@@ -1,37 +1,59 @@
+echo '#! /bin/bash' > salon.sh #create the file and print the shebang
+chmod +x salon.sh #grant executable permissions
+
+-------------------------
+#! /bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~"
+
 SERVICE_MENU(){
-    echo -e "\nwhich service would you like to appoint?\n"
-    SERVICES=$($PSQL "SELECT service_id, service_name FROM services")
-    echo $SERVICES | while read SERVICE_ID BAR SERVICE  
-    do
-        echo -e "$SERVICE_ID) $SERVICE"
-    done
-    
-    $SERVICE_ID=$($PSQL "SELECT service_id FROM services")
-    read SERVICE_ID_SELECTED
-    if [[ $SERVICE_ID_SELECTED != $SERVICE_ID ]]
-    then
-        SERVICE_MENU "Please select one of the available choices"
-    else
-    echo -e "\nPlease enter your phone number\n"
+
+  if [[ $1 ]] # print the text upfront the function, before returned to the initial part
+  then
+    echo -e "\n$1"
+  else
+    echo -e "\nWelcome to My Salon, how can I help you?\n"
+  fi
+
+  SERVICES=$($PSQL "SELECT service_id, name FROM services")
+
+  echo $SERVICES | while read SERVICE_ID1 BAR SERVICE1 SERVICE_ID2 BAR SERVICE2 SERVICE_ID3 BAR SERVICE3 SERVICE_ID4 BAR SERVICE4 SERVICE_ID5 BAR SERVICE5
+  do
+    echo -e "$SERVICE_ID1) $SERVICE1\n$SERVICE_ID2) $SERVICE2\n$SERVICE_ID3) $SERVICE3\n$SERVICE_ID4) $SERVICE4\n$SERVICE_ID5) $SERVICE5"
+  done
+
+  read SERVICE_ID_SELECTED
+
+  #SERV=$($PSQL "SELECT service_id FROM services")
+  #ARR=($SERV)
+  if [[ ! $SERVICE_ID_SELECTED =~ ([1-5]) ]]  
+  then
+    SERVICE_MENU "I could not find that service. What would you like today?"
+  else
+    echo -e "\nWhat's your phone number?"
     read CUSTOMER_PHONE
-    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
-        if [[ -z CUSTOMER_NAME ]]
-        then
-            echo "\nWhat is your name?"
-            read CUSTOMER_NAME
-            ADDED_CUSTOMER_NAME_PHONE=$($PSQL "ALTER TABLE customers(name, phone) VALUES ('$CUSTORMER_NAME', $PHONE)")
-        else
-            echo "What time would you like to schedule the appointment?"
-            read SERVICE_TIME
-            
-            if [[ ! $SERVICE TIME =~ ^[6-18]+$]]
-            then 
-                SERVICE_MENU "\nPlease enter an hour amont 6 to 18"
-            else
-                SCHE_TIME=$($PSQL "ALTER TABLE appointments(time) VALUES ($SERVICE_TIME)")
-            fi
-        SERVICE=$($PSQL "SELECT service FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-        echo -e "\nI have put you down for a $SERVICE at $SCHE_TIME, $CUSTOMER_NAME"
-        fi
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      ADD_CUSTOMER_NAME_PHONE=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+      SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+      echo -e "\nWhat time would you like your $SERVICE, $CUSTOMER_NAME"
+      read SERVICE_TIME
+    else
+      SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+      echo -e "\nWhat time would you like your"$SERVICE","$CUSTOMER_NAME""
+      read SERVICE_TIME
     fi
-}
+
+    CUSTOMER=$($PSQL "SELECT name FROM customers WHERE phone ='$CUSTOMER_PHONE'")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone ='$CUSTOMER_PHONE'")
+    INSERT_APP=$($PSQL "INSERT INTO appointments(time, customer_id, service_id) VALUES ('$SERVICE_TIME', $CUSTOMER_ID, $SERVICE_ID_SELECTED)")
+  fi
+  echo -e "\nI have put you down for a"$SERVICE" at "$SERVICE_TIME,$CUSTOMER"."
+  } 
+
+SERVICE_MENU
